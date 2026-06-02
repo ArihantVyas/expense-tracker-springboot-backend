f6b48d8567399337431b1ffb282482b0a8eb1511
ArihantVyas <arihantvyas@gmail.com>
2026-06-02 20:18:56 +0530

    Added the Security requirement with bearer to the OpenApiConfig

--- src/main/java/com/arihant/expense_tracker/config/OpenApiConfig.java
+++ src/main/java/com/arihant/expense_tracker/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package com.arihant.expense_tracker.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,6 +22,16 @@ public class OpenApiConfig{
                         .contact(new Contact()
                                 .name("Arihant Vyas")
                                 .email("arihantvyas.new.github@gmail.com")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication")
+                )
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
                         )
                 );
     }
